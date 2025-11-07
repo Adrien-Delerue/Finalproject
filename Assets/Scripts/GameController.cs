4f771336d43a0e0b2402c78610c7d4a956c5a528
Paul CAUCHE <paul.cauche@student.junia.com>
2025-11-07 23:02:21 +0100

    update gamecontroller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,7 +9,6 @@ public class GameController : MonoBehaviour
 
     void Awake()
     {
-        // Singleton pour éviter les duplicatas
         if (instance == null)
         {
             instance = this;
@@ -22,15 +21,12 @@ public class GameController : MonoBehaviour
 
     void Start()
     {
-        // Réinitialiser l'état au démarrage de la scène
         isGameOver = false;
         Time.timeScale = 1f;
 
-        // S'assurer que le crosshair est visible
         if (crossHair != null)
             crossHair.SetActive(true);
 
-        // S'assurer que le GameOver screen est caché
         if (GameOverScreen != null)
             GameOverScreen.gameObject.SetActive(false);
     }
@@ -42,7 +38,6 @@ public class GameController : MonoBehaviour
             isGameOver = true;
             Time.timeScale = 0f;
 
-            // Cacher le CrossHair
             if (crossHair != null)
                 crossHair.SetActive(false);
 
